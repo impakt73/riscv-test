2a6f2cae4b54a84c66efb43278cd60a9a761cd2e
Gregory Mitrano <gregory.mitrano@gmail.com>
2020-07-30 04:06:31 -0400

    Updated Test Executable to Work on Latest HW
    This change updates the test elf to query the FB address and size from
    registers and also changes the test pattern a bit.

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,34 @@ use riscv_rt::entry;
 // `main` is not allowed to return
 #[entry]
 fn main() -> ! {
-    let base_ptr = 0x1F00 as *mut u8;
-    let image_data = unsafe { core::slice::from_raw_parts_mut(base_ptr, 256) };
+    let reg_base_ptr = 0x100000 as *const u32;
+    let regs = unsafe { core::slice::from_raw_parts(reg_base_ptr, 1024) };
+
+    let fb_base_addr = regs[1] as *mut u8;
+
+    let fb_width_log2 = (regs[2] & 0x7) + 1;
+    let fb_height_log2 = ((regs[2] >> 3) & 0x7) + 1;
+
+    let fb_width = 1 << fb_width_log2;
+    let fb_height = 1 << fb_height_log2;
+
+    let fb_data = unsafe { core::slice::from_raw_parts_mut(fb_base_addr, (fb_width * fb_height) as usize) };
 
     loop {
-        for y in 0..16 {
-            for x in 0..16 {
-                let idx = (y << 4) + x;
+        for y in 0..fb_height {
+            for x in 0..fb_width {
+                let idx = y * fb_width + x;
                 //let color = ((x >> 2) & 1) * 255;
                 //let color = if (x & y) != 0 { 255 } else { 0 };
                 //let lit = ((x >> 2) & 1) ^ ((y >> 2) & 1);
 
                 //image_data[idx] = if lit != 0 { 255 } else { 0 };
-                image_data[idx] = (if (((x >> 2) & 1) ^ ((y >> 2) & 1)) != 0 {
-                    idx
+                fb_data[idx as usize] = (if (((x >> 2) & 1) ^ ((y >> 2) & 1)) != 0 {
+                    idx & 255
                 } else {
                     0
                 }) as u8;
+                //image_data[idx] = (idx & 255) as u8;
             }
         }
 
