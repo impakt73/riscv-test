6ae1280c2a15c875db072ebc85c108f8df699440
Chris Butler <chrisbutler296@gmail.com>
2020-08-21 12:27:25 -0400

    Add a `sim` runner
    With this you can run `cargo run` and have it behave correctly.

---
+++ runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Cargo gives us a relative path to the binary.
+# We save it here as an abs path so that we can reference it after moving to
+# the devsim dir.
+elf_path="$(realpath $1)"
+
+pushd ../devsim/
+cargo run --release \
+    --bin sim       \
+    -- $elf_path
+
+# WA: devsim doesn't have an image output option, so it dumps it to $PWD.
+img_path="$(realpath "./image.png")"
+popd
+
+cp -v $img_path .
